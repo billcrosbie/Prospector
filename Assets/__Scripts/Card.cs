3fb89c1b249404d8a8806a6ffa5f29820871f90b
kasiawawer <kew114@scarletmail.rutgers.edu>
2015-11-12 18:08:08 -0800

    yay

--- Assets/__Scripts/Card.cs
+++ Assets/__Scripts/Card.cs
@@ -13,16 +13,66 @@ public class Card : MonoBehaviour {
 	public List<GameObject> pipGOs = new List<GameObject>();
 	
 	public GameObject back;  // back of card;
-	public CardDefinition def;  // from DeckXML.xml		
-
+	public CardDefinition def;  // from DeckXML.xml	
+	public SpriteRenderer[] spriteRenderers;
+	
 	// Use this for initialization
 	void Start () {
-	
+		SetSortOrder (0);
+		
+	}
+
+	public bool faceUp {
+		get {
+			return(!back.activeSelf);
+		}
+		set {
+			back.SetActive (!value);
+		}
+	}
+
+	public void PopulateSpriteRenderers(){
+		if (spriteRenderers == null || spriteRenderers.Length == 0) {
+			spriteRenderers = GetComponentsInChildren<SpriteRenderer> ();
+		}
+	}
+
+	public void SetSortingLayerName(string tSLN){
+		PopulateSpriteRenderers ();
+
+		foreach (SpriteRenderer tSR in spriteRenderers) {
+			tSR.sortingLayerName = tSLN;
+		}
+	}
+
+	public void SetSortOrder (int sOrd){
+		PopulateSpriteRenderers ();
+
+		foreach(SpriteRenderer tSR in spriteRenderers){
+			if (tSR.gameObject == this.gameObject){
+				tSR.sortingOrder = sOrd;
+				continue;
+			}
+			switch (tSR.gameObject.name){
+			case "back": 
+				tSR.sortingOrder = sOrd + 2;
+				break;
+
+			case "face":
+			default:
+				tSR.sortingOrder = sOrd + 1;
+				break;
+			}
+		}
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
+	}
+
+	virtual public void OnMouseUpAsButton(){
+		print (name);
 	}
 } // class Card
 
@@ -38,6 +88,5 @@ public class Decorator{
 public class CardDefinition{
 	public string	face;	//sprite to use for face cart
 	public int		rank;	// value from 1-13 (Ace-King)
-	public List<Decorator>	
-					pips = new List<Decorator>();  // Pips Used
+	public List<Decorator>	pips = new List<Decorator>();  // Pips Used
 }
--- Assets/__Scripts/Deck.cs
+++ Assets/__Scripts/Deck.cs
@@ -235,7 +235,18 @@ public class Deck : MonoBehaviour {
 				tGO.transform.localPosition = Vector3.zero;  // slap it smack dab in the middle
 				tGO.name = "face";
 			}
-			
+
+			tGO = Instantiate(prefabSprite) as GameObject;
+			tSR = tGO.GetComponent<SpriteRenderer>();
+			tSR.sprite = cardBack;
+			tGO.transform.parent = card.transform;
+			tGO.transform.localPosition = Vector3.zero;
+			tSR.sortingOrder = 2;
+			tGO.name = "back";
+			card.back = tGO;
+
+			card.faceUp = true;
+
 			cards.Add (card);
 		} // for all the Cardnames	
 	} // makeCards
@@ -249,5 +260,18 @@ public class Deck : MonoBehaviour {
 		}//foreach	
 		return (null);  // couldn't find the sprite (should never reach this line)
 	 }// getFace 
-	
+
+	static public void Shuffle(ref List<Card> oCards){
+		List<Card> tCards = new List<Card> ();
+		int ndx;
+		tCards = new List<Card> ();
+
+		while (oCards.Count > 0) {
+			ndx = Random.Range (0, oCards.Count);
+			tCards.Add (oCards [ndx]);
+			oCards.RemoveAt (ndx);
+		}
+		oCards = tCards;
+	}
+
 } // Deck class
